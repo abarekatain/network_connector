57a411f5b1ff3655b7bb739c2c9176cb3c1056f5
abarekatain <abarekatain96@gmail.com>
2020-03-09 12:19:14 -0400

    Initialized launch file and parameters

--- scripts/autobahn_connector.py
+++ scripts/autobahn_connector.py
@@ -14,9 +14,18 @@ url = u'ws://localhost:8080/ws'
 realmv = u'realm1'
 component = Component(transports=url, realm=realmv)
 
+def get_parameters():
+    global pub2net_topics , sub2net_topics
+    pub2net_topics = rospy.get_param("pub2net_topics")
+    sub2net_topics = rospy.get_param("sub2net_topics")
+
+
 if __name__ == "__main__":
     rospy.init_node('autobahn_connector', anonymous=False)
-    run([component])
+    get_parameters()
+    for topic in sub2net_topics:
+        print (topic)
+    #run([component])
     #while not rospy.is_shutdown():
     #    rospy.spin()
     print("main gone")
