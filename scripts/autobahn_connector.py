02b9afeb29bf8ab286139dddd4dbb9593127f0bb
abarekatain <abarekatain96@gmail.com>
2020-03-10 13:57:09 -0400

    Integrated with RisBridge and Updated CmakeList and Package XML

--- scripts/autobahn_connector.py
+++ scripts/autobahn_connector.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 
 from autobahn.twisted.component import Component, run
@@ -9,6 +9,13 @@ import argparse
 import six
 import rospy
 from std_msgs.msg import String
+from rostopic import get_topic_type
+
+from rosbridge_library.internal import ros_loader, message_conversion
+from rosbridge_library.internal.topics import TopicNotEstablishedException
+from rosbridge_library.internal.topics import TypeConflictException
+from rosbridge_library.internal.outgoing_message import OutgoingMessage
+
 
 url = u'ws://localhost:8080/ws'
 realmv = u'realm1'
@@ -20,12 +27,29 @@ def get_parameters():
     sub2net_topics = rospy.get_param("sub2net_topics")
 
 
+def callback(data):
+    rospy.loginfo(data.data)
+
+
+
 if __name__ == "__main__":
     rospy.init_node('autobahn_connector', anonymous=False)
     get_parameters()
-    for topic in sub2net_topics:
-        print (topic)
+
+    rospy.Subscriber("/test", String, lambda data: callback(data))
+
+    topic_type = get_topic_type("/test")[0]
+
+    print(topic_type)
+
+
+
+
+
+
+    #for topic in sub2net_topics:
+    #    print (topic)
     #run([component])
-    #while not rospy.is_shutdown():
-    #    rospy.spin()
+    while not rospy.is_shutdown():
+        rospy.spin()
     print("main gone")
