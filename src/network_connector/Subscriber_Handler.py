555362c288d6e6075bc6fcff7ca47a3914663c89
abarekatain <alireza.barekatain1996@gmail.com>
2020-03-14 04:22:18 -0400

    Pub2Net basic implementation successful

--- src/network_connector/Subscriber_Handler.py
+++ src/network_connector/Subscriber_Handler.py
@@ -1,17 +1,29 @@
 import rospy
+from rospy_message_converter import json_message_converter
+from std_msgs.msg import *
+import json
 
 class Subscriber_Handler():
 
     session = None
+    
 
     def __init__(self, topic_name, topic_type):
         self.topic_name = topic_name
         self.topic_type = topic_type
         self.robotID = rospy.get_param("robotID")
+        self.network_topic_name = "com.{}.robot".format(self.robotID)
         #self.session = clientSession
         
 
     def callback(self,message):
-        Subscriber_Handler.session.publish('com.myapp.hello', self.topic_name)
+
+        json_str = json_message_converter.convert_ros_message_to_json(message)
+        json_str = json.loads(json_str)
+        json_str['topic_name'] = self.topic_name
+        json_str['topic_type'] = self.topic_type
+        json_str = json.dumps(json_str)
+        #print(json_str)
+        Subscriber_Handler.session.publish(self.network_topic_name, json_str)
         #print(type(self.session))
         
